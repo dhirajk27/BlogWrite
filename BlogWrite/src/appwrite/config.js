8dbc1a2d550f3cfc27851abf3017e0b6603fef30
Dhiraj <dheerajkhandave9552@gmail.com>
2024-06-10 22:41:56 +0530

    config.js

--- BlogWrite/src/appwrite/config.js
+++ BlogWrite/src/appwrite/config.js
@@ -96,33 +96,33 @@ export class Service{
         }
     }
 
-    // file upload service
+    // // file upload service
 
-    async uploadFile(file){
-        try {
-            return await this.bucket.createFile(
-                conf.appwriteBucketId,
-                ID.unique(),
-                file
-            )
-        } catch (error) {
-            console.log("Appwrite serive :: uploadFile :: error", error);
-            return false
-        }
-    }
+    // async uploadFile(file){
+    //     try {
+    //         return await this.bucket.createFile(
+    //             conf.appwriteBucketId,
+    //             ID.unique(),
+    //             file
+    //         )
+    //     } catch (error) {
+    //         console.log("Appwrite serive :: uploadFile :: error", error);
+    //         return false
+    //     }
+    // }
 
-    async deleteFile(fileId){
-        try {
-            await this.bucket.deleteFile(
-                conf.appwriteBucketId,
-                fileId
-            )
-            return true
-        } catch (error) {
-            console.log("Appwrite serive :: deleteFile :: error", error);
-            return false
-        }
-    }
+    // async deleteFile(fileId){
+    //     try {
+    //         await this.bucket.deleteFile(
+    //             conf.appwriteBucketId,
+    //             fileId
+    //         )
+    //         return true
+    //     } catch (error) {
+    //         console.log("Appwrite serive :: deleteFile :: error", error);
+    //         return false
+    //     }
+    // }
 
     getFilePreview(fileId){
         return this.bucket.getFilePreview(

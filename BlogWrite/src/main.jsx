99f80185002e7d7db4c2f11722c1213ef6f42b00
Dhiraj <dheerajkhandave9552@gmail.com>
2024-06-11 00:41:38 +0530

    main.jsx

--- BlogWrite/src/main.jsx
+++ BlogWrite/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store/store.js'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import { AuthLayout, Login } from './components/index.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )

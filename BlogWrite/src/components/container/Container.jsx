36541d1a3e7252780056dc9fdbc547e1a72aaf80
Dhiraj <dheerajkhandave9552@gmail.com>
2024-07-06 23:36:01 +0530

    container

--- BlogWrite/src/components/container/Container.jsx
+++ BlogWrite/src/components/container/Container.jsx
@@ -1,9 +1,8 @@
-import React, { Children } from 'react'
+import React from 'react'
 
-export const Container = () => {
-  return (
-    <div className='w-full max-w-7xl mx-auto'>{Children}</div>
-  )
+function Container({children}) {
+  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+  
 }
 
 export default Container

d173cf036eaf63e69245d1e5cf250a201539adce
Dhiraj <dheerajkhandave9552@gmail.com>
2024-07-12 17:25:13 +0530

    conf.js

--- BlogWrite/src/conf/conf.js
+++ BlogWrite/src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf ={
-    blogwriteUrl: String(import.meta.env.VITE_BLOGWRITE_URL),
-    blogwriteProjectId: String(import.meta.env.VITE_BLOGWRITE_PROJECT_ID),
-    blogwriteDatabaseId: String(import.meta.env.VITE_BLOGWRITE_DATABASE_ID),
-    blogwriteCollectionId: String(import.meta.env.VITE_BLOGWRITE_COLLECTION_ID),
-    blogwriteBucketId: String(import.meta.env.VITE_BLOGWRITE_BUCKET_ID),
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
 }
 
 
